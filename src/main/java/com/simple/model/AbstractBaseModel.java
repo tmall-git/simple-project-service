b87167249115626a9969e372fd34528bbadf79e0
zhengfy1 <zhengfy1@lenovo.com>
2016-09-03 17:37:18 +0800

    update

--- src/main/java/com/simple/model/AbstractBaseModel.java
+++ src/main/java/com/simple/model/AbstractBaseModel.java
@@ -1,7 +1,12 @@
 package com.simple.model;
 
+import com.simple.common.config.EnvPropertiesConfiger;
+
 public class AbstractBaseModel implements java.io.Serializable{
 
+	//private static final String FILE_PREFIX = EnvPropertiesConfiger.getValue("fileUrl");
+	private static final String FILE_PREFIX = "http://juxier.com/file/";
+	
 	private static final long serialVersionUID = -522832123597167225L;
 	
 	protected int id;
@@ -17,6 +22,23 @@ public class AbstractBaseModel implements java.io.Serializable{
 	public AbstractBaseModel() {
 	}
 	
-	
-	
+	public String getImagePath(String path) {
+		if (null != path) {
+			if (!path.startsWith("http://")) {
+				path = path.replace("\\", "/").replaceAll("/{2,}", "/");
+				if (path.startsWith("/")) {
+					path = path.substring(1);
+				}
+				path = FILE_PREFIX+path;
+			}else {
+				path = path.replace("http://", "");
+				path = path.replace("\\", "/").replaceAll("/{2,}", "/");
+				if (path.startsWith("/")) {
+					path = path.substring(1);
+				}
+				path = "http://"+path;
+			}
+		}
+		return path;
+	}
 }
